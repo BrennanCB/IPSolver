f024a23c12a8fba0a6acf4ab7f6fe066ef85a36f
Cobus Viviers <Cobusv11@gmail.com>
2017-11-07 20:17:46 +0200

    Added Special Case Exceptions

--- IPSolver/IPSolver/LpTools.cs
+++ IPSolver/IPSolver/LpTools.cs
@@ -8,5 +8,32 @@ namespace IPSolver
 {
     class LpTools
     {
+        public static bool CheckIfLpIsSolved(LinearProgram LinearProgram)
+        {
+            if (CheckSpecialCases(LinearProgram))
+                return false;
+
+            double[,] problemMatrix = LinearProgram.LinearProgramArray;
+            
+            for (int i = 0; i < LinearProgram.ColumnCount; i++)
+            {
+                if (problemMatrix[0, i] < 0)
+                    return false;
+            }
+
+            
+            for (int j = 0; j < LinearProgram.RowCount; j++)
+            {
+                if (problemMatrix[j, LinearProgram.ColumnCount - 1] < 0)
+                    return false;
+            }
+
+
+        }
+
+        public static bool CheckSpecialCases(LinearProgram LinearProgram)
+        {
+            return false;
+        }
     }
 }
