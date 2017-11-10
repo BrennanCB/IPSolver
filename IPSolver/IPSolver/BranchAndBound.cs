13a5f629d0297e14bf62fb788fcfa83c43730055
Cobus Viviers <Cobusv11@gmail.com>
2017-11-11 00:10:38 +0200

    Branch and bound solution tracking
    Added the logic to keep track of branch and bound solutions, still needs logic to solve each individual problem

--- IPSolver/IPSolver/BranchAndBound.cs
+++ IPSolver/IPSolver/BranchAndBound.cs
@@ -9,6 +9,65 @@ namespace IPSolver
     class BranchAndBound
     {
 
+        private class ProblemNode
+        {
+            LinearProgram problem;
+            bool solved;
+            double[] xValues;
+            double zValue;
+
+            public ProblemNode(LinearProgram problem, bool solved, double[] xValues, double zValue)
+            {
+                this.Problem = problem;
+                this.Solved = solved;
+                this.XValues = xValues;
+                this.ZValue = zValue;
+            }
+
+            public LinearProgram Problem { get => problem; set => problem = value; }
+            public bool Solved { get => solved; set => solved = value; }
+            public double[] XValues { get => xValues; set => xValues = value; }
+            public double ZValue { get => zValue; set => zValue = value; }
+        }
+
+        private List<ProblemNode> problems;
+
+        public BranchAndBound()
+        {
+            problems = new List<ProblemNode>();
+        }
+
+        public LinearProgram Sovle(LinearProgram problem, LPType type)
+        {
+            bool solved = false;
+            ProblemNode currentOptimal = problems.ElementAt(0);
+
+            while (true)
+            {
+                ProblemNode currentProblem = problems.ElementAt(0);
+                for (int i = 0; i < problems.Count; i++)
+                {
+                    if (!problems.ElementAt(i).Solved)
+                        break;
+                    if (problems.Count == i - 1)
+                        solved = true;
+                }
+
+                if (solved)
+                    break;
+
+                //todo: logic to solve here
+                //todo: check for infinte loop when initial problem cannot be solved
+
+                if ((type == LPType.Max && currentProblem.ZValue > currentOptimal.ZValue)
+                    || (type == LPType.Min && currentOptimal.ZValue > currentProblem.ZValue))
+                { 
+                    currentOptimal = currentProblem;
+                }
+            }
+            return currentOptimal.Problem;
+        }
+
 
     }
 }
