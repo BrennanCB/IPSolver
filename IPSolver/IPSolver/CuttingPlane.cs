a34d7c5b3219fdfcbaecf8327fe645f3f25e0e4f
Cobus Viviers <Cobusv11@gmail.com>
2017-11-12 02:53:09 +0200

    Cutting plane start
    I'll do this tomorrow

---
+++ IPSolver/IPSolver/CuttingPlane.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPSolver
+{
+    class CuttingPlane
+    {
+        LinearProgram linearProgram;
+        public CuttingPlane(LinearProgram linearProgram)
+        {
+            this.linearProgram = linearProgram;
+        }
+        public LinearProgram Solve()
+        {
+            return linearProgram;
+        }
+    }
+}
